bbcc1ca21a37d86298e6d78f946bd77f9b92bec5
Luciana <luciana.gadelhaa@gmail.com>
2015-12-22 02:56:50 -0200

    Atividade

---
+++ InterfacePessoa/src/main/java/PessoaI.java
@@ -0,0 +1,19 @@
+
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Luciana
+ */
+public interface PessoaI {
+
+    public void cadastrar(String nome);
+
+    public List<String> listarPessoa();
+
+}
